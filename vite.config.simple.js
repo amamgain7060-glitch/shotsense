a5c151c5c9b0a0e35ef5b69adb8152451f4d8763
Anirudh Mamgain <anirudh@shotsense.com>
2025-10-23 14:07:01 +0530

    Clean up: remove empty Shotsense folder

---
+++ vite.config.simple.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+})
